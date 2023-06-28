783d491400a1180e9ff03fe9365183c2be806894
Bryce Fischer <bryce-fischer@idexx.com>
2023-06-28 09:58:06 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="card w-96 card-bordered bg-base-100 shadow-xl px-lg">
+            <div className="card-body">
+              <p>Data card</p>
+            </div>
+          </div>
+          <div className="card w-96 card-bordered bg-base-100 shadow-xl px-lg">
+            <div className="card-body">
+              <p>Data card</p>
+            </div>
+          </div>
+          <div className="card w-96 card-bordered bg-base-100 shadow-xl px-lg">
+            <div className="card-body">
+              <p>Data card</p>
+            </div>
+          </div>
+          <div className="card w-96 card-bordered bg-base-100 shadow-xl px-lg">
+            <div className="card-body">
+              <p>Data card</p>
+            </div>
+          </div>
+          <div className="card w-96 card-bordered bg-base-100 shadow-xl px-lg">
+            <div className="card-body">
+              <p>Data card</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,6 @@
+import { CardProps } from "./Card.types";
+import "./Cards.css";
+
+export const Card = (props: CardProps) => {
+  return <div>Card Contents</div>;
+};
